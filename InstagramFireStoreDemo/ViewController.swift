249d237b9eeb0e0836c9b7b5faf75ddd9271dd38
Yosuke Sakurai <hawk20605@gmail.com>
2022-03-29 15:30:18 -0500

    Initial Commit

---
+++ InstagramFireStoreDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFireStoreDemo
+//
+//  Created by Yosuke Sakurai on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
